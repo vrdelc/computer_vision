0e0b4c047619b22390e2c8313e24b45f49921544
Virginia Riego del Castillo <virginiardelc@gmail.com>
2019-04-28 19:09:45 +0200

    Requires

---
+++ requires.py
@@ -0,0 +1,16 @@
+import pip
+import subprocess
+import sys
+
+def install(package):
+    if hasattr(pip, 'main'):
+        pip.main(['install', package])
+
+if __name__ == '__main__':
+    reqs = subprocess.check_output([sys.executable, '-m', 'pip', 'freeze'])
+    installed_packages = [r.decode().split('==')[0] for r in reqs.split()]
+    packets = ['crawler','python-pixabay','numpy','scipy','matplotlib','sklearn']
+    for packet in packets:
+        if packet not in installed_packages:
+            print("Installing packet {}".format(packet))
+            install(packet)
