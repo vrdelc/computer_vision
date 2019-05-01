3f2b45c7106aa2ddb549a0d5f490ff7fe5bfa648
Virginia Riego del Castillo <virginiardelc@gmail.com>
2019-05-01 14:50:21 +0200

    Predictions, confusion matrix and metrics

--- classifier.py
+++ classifier.py
@@ -18,10 +18,29 @@ class Classifier:
         # Train the Linear SVM
         self.clf = LinearSVC()
         X_train, self.X_test, y_train, self.y_test = train_test_split(self.histograms, self.classes, random_state=0)
+        print("{} training and {} test".format(len(X_train),len(self.X_test)))
         X = self.clf.fit(X_train,y_train)
         # Save the SVM
         joblib.dump((self.clf, self.types_classes, self.histogramClasses[1], self.X_test, self.y_test), "bof.pkl", compress=3)
 
     def score(self):
-        score = self.clf.score(self.X_test,self.y_test)
-        print("Mean accuracy to classifier: {:2.2f}%".format(score*100))
+        predictions = self.clf.predict(self.X_test)
+        confussion_matrix = [[0,0],[0,0]]
+        for i in range(len(self.y_test)):
+            if self.y_test[i] == 0: #gambling
+                if predictions[i] == 0: #TP
+                    confussion_matrix[0][0] = confussion_matrix[0][0] +1
+                else: #FN
+                    confussion_matrix[1][0] = confussion_matrix[1][0] +1
+            else: #non-gambling
+                if predictions[i] == 0: #FP
+                    confussion_matrix[0][1] = confussion_matrix[0][1] +1
+                else: #TN
+                    confussion_matrix[1][1] = confussion_matrix[1][1] +1
+        print("Confusion matrix gambling: {}".format(confussion_matrix))
+        precision = confussion_matrix[0][0] / (confussion_matrix[0][0]+confussion_matrix[0][1])
+        recall = confussion_matrix[0][0] / (confussion_matrix[0][0]+confussion_matrix[1][0])
+        f1_score = (2*precision*recall) / (precision+recall)
+        print("Precision {:2.4f}".format(precision))
+        print("Recall {:2.4f}".format(recall))
+        print("F1 Score {:2.4f}".format(f1_score))
--- main_deep.py
+++ main_deep.py
@@ -7,7 +7,7 @@ from dataset_deep import Dataset
 
 folder_dataset = 'C:/dataset/'
 
-folder_dataset = 'crawler/'
+#folder_dataset = 'crawler/'
 
 IMG_SIZE = 700
 
@@ -16,31 +16,4 @@ dataset = Dataset(img_size=IMG_SIZE)
 dataset.show_statics()
 dataset.prepare_data()
 dataset.generate_model()
-
-"""
-# Test on Test Set
-TEST_DIR = './test'
-def load_test_data():
-    test_data = []
-    for img in os.listdir(TEST_DIR):
-        label = label_img(img)
-        path = os.path.join(TEST_DIR, img)
-        if "DS_Store" not in path:
-            img = Image.open(path)
-            img = img.convert('L')
-            img = img.resize((IMG_SIZE, IMG_SIZE), Image.ANTIALIAS)
-            test_data.append([np.array(img), label])
-    shuffle(test_data)
-    return test_data
-
-
-test_data = load_test_data()
-plt.imshow(test_data[10][0], cmap = 'gist_gray')
-
-testImages = np.array([i[0] for i in test_data]).reshape(-1, IMG_SIZE, IMG_SIZE, 1)
-testLabels = np.array([i[1] for i in test_data])
-
-loss, acc = model.evaluate(testImages, testLabels, verbose = 0)
-print(acc * 100)
-
-"""
+dataset.evaluate()
